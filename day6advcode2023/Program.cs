d2b95efe4d0f37e1d81469825d3f4d2bfc58e5be
Mohamud Nur <Mohamud-99@hotmail.com>
2023-12-14 20:28:42 +0100

    Add project files.

---
+++ day6advcode2023/Program.cs
@@ -0,0 +1,46 @@
+﻿
+string filePath = "input.txt";
+string[] lines = File.ReadAllLines(filePath);
+var timeInfo = lines[0].Split(":");
+
+var times = timeInfo[1].Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                       .Select(int.Parse)
+                       .ToArray();
+
+var distanceInfo = lines[1].Split(":");
+var distance = distanceInfo[1].Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                              .Select(int.Parse)      
+                              .ToArray();    // GPT hjälpte mig här med att konvertera till int och array men är det ett sätt att skriva på
+                                             // alltså på samma linje med metoderna
+int speed = 0;
+int works = 0;
+int i = 0;
+int result = 1;
+
+foreach (int time in times)
+{
+   int working = time;
+   while (working != 0)
+   {
+     if ((working * speed) <= distance[i]) 
+     {
+        speed += 1;
+        working -= 1;
+      }
+     else
+     {
+        works += 1;
+        speed += 1;
+        working -= 1;
+     }
+     if (working == 0) { break; }
+   }
+    i++;
+    result *= works;
+    speed = 0;
+    works = 0;
+}
+Console.WriteLine(result);
+
+// Denna tog ganska lång tid för mig men mest för att jag inte kunde få distans o tid på samma
+// alltså när time är [0] då ta från distance [0] men lyckades med det tillslut.
